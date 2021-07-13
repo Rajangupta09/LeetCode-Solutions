8430519984e15cca4dc48310b38d234a9e0ba76a
Rajangupta09 <rjnkumar05@gmail.com>
2021-07-13 11:15:48 +0530

    [ADD] number-of-1-bits.java

---
+++ number-of-1-bits.java
@@ -0,0 +1,10 @@
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        if (n==0){
+            return 0;
+        }else{
+            return 1+ hammingWeight(n &(n-1));
+        }
+    }
+}
