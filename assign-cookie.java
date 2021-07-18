7a218786a4aed1ebdc8e75190cc6723635c152e0
Rajangupta09 <rjnkumar05@gmail.com>
2021-07-18 13:47:51 +0530

    [ADD] assign-cookie.java

---
+++ assign-cookie.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);Arrays.sort(s);
+        int i = 0,j=0;
+        int ans = 0;
+        while(i!=s.length&&j<g.length){
+            if(s[i]>=g[j]){
+                i++;j++;ans++;
+            }else{
+                i++;
+            }
+        }
+        return ans;
+    }
+}
