92aeadf7d8d4eb9f7bf1fcdb739697c2bee144d0
Rajangupta09 <rjnkumar05@gmail.com>
2021-07-20 16:03:22 +0530

    [ADD] perfect-number.java

---
+++ perfect-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if (num<3)return false;
+        int sum =1;
+        for(int i =2;i*i<num;i++){
+            if(num%i ==0){
+                sum+=i+(num/i);
+            }
+        } 
+        return sum==num;
+    }
+}
