63e86f6517804d85cf407d3e2fb84fd2908295bc
Rajangupta09 <rjnkumar05@gmail.com>
2021-07-22 08:41:48 +0530

    [ADD] detect-capital.java

---
+++ detect-capital.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int n = word.length();
+        if(n==1)return true;
+        if(Character.isUpperCase(word.charAt(0))){
+            if(Character.isUpperCase(word.charAt(1))){
+                for(int i=2;i<n;i++){
+                    
+                    if(Character.isUpperCase(word.charAt(i))==false)return false;
+                }
+            }else{
+                for(int i=2;i<n;i++){
+                    if(Character.isUpperCase(word.charAt(i)))return false;
+                }
+            }
+        }else{
+            for(int i=1;i<n;i++){
+                if(Character.isUpperCase(word.charAt(i)))return false;
+            }
+            
+        }
+        return true;
+    
+    }
+}
