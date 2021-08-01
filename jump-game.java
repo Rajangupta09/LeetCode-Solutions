eb174848d82e9e2b811e98bd27d599033c150b91
Rajangupta09 <rjnkumar05@gmail.com>
2021-08-01 12:14:25 +0530

    [ADD] jump-game.java

---
+++ jump-game.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+     int[] dp = new int[nums.length];
+        dp[0] = 0;
+        for(int i=1;i<nums.length;i++){
+            dp[i] = Integer.MAX_VALUE;
+        }
+        for(int i=1;i<nums.length;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]+j>=i){
+                    if(dp[j]!=Integer.MAX_VALUE){
+                        dp[i] = Math.min(dp[i], dp[j]+1);
+                    }
+                }
+            }
+        }
+        return dp[nums.length-1]!=Integer.MAX_VALUE;
+    }
+}
